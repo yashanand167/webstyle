471eb5de64ff3578499f147bec6be31357a6cb8f
yashanand167 <yash.anand167@gmail.com>
2026-07-31 23:06:43 +0530

    Initialize project with Tailwind CSS and React setup; add popup UI structure and dependencies.

---
+++ src/popup/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </>
+  )
+}
+
+//goal: cook my own UI browser extension
+//next level frontend engineering no one has ever done in my contacts before
+
+export default App
